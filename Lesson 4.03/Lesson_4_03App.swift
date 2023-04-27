aedefacc391d16387e78e11036d0cb08604fa78b
Liza Grossman <lisok.sam@gmail.com>
2023-04-27 18:26:44 +0300

    Initial Commit

---
+++ Lesson 4.03/Lesson_4_03App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_03App.swift
+//  Lesson 4.03
+//
+//  Created by Elizabeth on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
